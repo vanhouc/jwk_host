522fc408053a8293aed27f656409c5f2e2b11dc1
Cameron VanHouzen <cameron@vanhouzen.me>
2026-02-19 13:15:44 -0500

    Support RSA Keys (#1)

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 ```
 openssl ecparam -name secp384r1 -genkey -noout | openssl pkcs8 -topk8 -nocrypt
 ```
+
+# Generating a valid RSA4096 PKCS#8 Key
+```
+openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:4096 | openssl pkcs8 -topk8 -nocrypt
+```
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ async fn main() {
     #[cfg(debug_assertions)]
     dotenvy::dotenv().ok();
 
+    for (key, value) in std::env::vars() {
+        println!("{key}: {value}");
+    }
+
     // Load JWK set from environment variables
     let jwk_set = jwk_set_from_env();
 
@@ -21,18 +25,34 @@ async fn main() {
 fn jwk_set_from_env() -> jsonwebtoken::jwk::JwkSet {
     let keys = std::env::vars()
         .filter(|(key, _)| key.starts_with("PRIVATE_KEY"))
-        .map(|(_, value)| {
-            println!("{value}");
-            let encoding_key = jsonwebtoken::EncodingKey::from_ec_pem(value.as_bytes())
-                .expect("Encoding Key must be a valid EC PKCS#8 PEM Key");
-            let mut jwk = jsonwebtoken::jwk::Jwk::from_encoding_key(
-                &encoding_key,
-                jsonwebtoken::Algorithm::ES384,
-            )
-            .unwrap();
-            // from_encoding_key does not set the key ID so set it manually
-            jwk.common.key_id = Some(jwk.thumbprint(jsonwebtoken::jwk::ThumbprintHash::SHA256));
-            jwk
+        .map(|(key, value)| {
+            if key.contains("RSA") {
+                println!("{value}");
+                let encoding_key = jsonwebtoken::EncodingKey::from_rsa_pem(value.as_bytes())
+                    .expect("Encoding Key must be a valid RSA PKCS#8 PEM Key");
+                let mut jwk = jsonwebtoken::jwk::Jwk::from_encoding_key(
+                    &encoding_key,
+                    jsonwebtoken::Algorithm::PS512,
+                )
+                .unwrap();
+                // from_encoding_key does not set the key ID so set it manually
+                jwk.common.key_id = Some(jwk.thumbprint(jsonwebtoken::jwk::ThumbprintHash::SHA256));
+                jwk
+            } else if key.contains("EC") {
+                println!("{value}");
+                let encoding_key = jsonwebtoken::EncodingKey::from_ec_pem(value.as_bytes())
+                    .expect("Encoding Key must be a valid EC PKCS#8 PEM Key");
+                let mut jwk = jsonwebtoken::jwk::Jwk::from_encoding_key(
+                    &encoding_key,
+                    jsonwebtoken::Algorithm::ES384,
+                )
+                .unwrap();
+                // from_encoding_key does not set the key ID so set it manually
+                jwk.common.key_id = Some(jwk.thumbprint(jsonwebtoken::jwk::ThumbprintHash::SHA256));
+                jwk
+            } else {
+                panic!("Unsupported key type: {key}");
+            }
         });
 
     // Collect the keys into a JWK set
